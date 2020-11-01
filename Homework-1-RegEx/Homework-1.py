d5cc67e31fde754524cbccc90028c968761b9886
glaswasser <glaswasser@gmail.com>
2020-11-01 14:25:46 +0100

    first working version

--- Homework-1-RegEx/Homework-1.py
+++ Homework-1-RegEx/Homework-1.py
@@ -20,14 +20,17 @@ print(ip.group(0))
 
 # 2. Write a pattern that matches all occurrences of the word “visit” (i.e. “visits”, “visited”...)
 
+# directory containing text files:
 directory = "/Users/alexanderheinz/Library/Mobile Documents/com~apple~CloudDocs/UIBK/Data Analysis 2/homework-1/"
 
+# get file names
 file_names = os.listdir(directory)
 
+# get content
 books = str()
 for file in file_names:
     new = open(directory + file, "r").read()
-    books = books+new
+    books = books + new
 
 # now we want to detect the pattern of the word "visit"
 
@@ -45,6 +48,42 @@ print(result)
 # https://mkyong.com/regular-expressions/domain-name-regular-expression-example/
 domain = "www.mkyong.com"
 
-name = re.search("^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$” ", domain)
+name = re.search("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$", domain)
 
-print(name)
+print(name.group(0))
+
+print(re.search("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$", "mkyong,com"))
+
+
+
+# Search in internet for a pattern for email addresses.
+# https://stackoverflow.com/questions/8022530/how-to-check-for-valid-email-address
+
+email = "glaswasser@gmail.com"
+
+if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+    print("VALID MAIL ADDRESS")
+
+
+# Find a pattern for Austrian phone numbers and replace it with the normalized way of phone numbers.
+# (I.e. “0512 507-45010” should become “+43 (512) 507-45010”
+
+number = "0512 507-45010"
+
+def replace_number(number):
+
+    digits = re.findall(r'\S+', number)
+
+    digits[0] = re.sub(r"^0", r"+43 (", digits[0])
+
+    digits[0] = re.sub(r"$", r")", digits[0])
+
+    new_number = " ".join(digits)
+
+    return new_number
+
+
+print(replace_number(number))
+
+print(replace_number("0532 15-9273"))
+
